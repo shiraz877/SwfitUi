a80ba39ba1eeed87d8c0965f3b9ce0eba5f6183f
shiraz877 <shiraz@sofyrus.com>
2026-07-25 14:03:15 +0530

    Initial Commit

---
+++ Swfitui/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Swfitui
+//
+//  Created by Shiraz on 25/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Swfitui/SwfituiApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwfituiApp.swift
+//  Swfitui
+//
+//  Created by Shiraz on 25/07/26.
+//
+
+import SwiftUI
+
+@main
+struct SwfituiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
