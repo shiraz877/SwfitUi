1339a5bd113308c0d09ff96717370b950aeb3268
shiraz877 <shiraz@sofyrus.com>
2026-07-25 14:58:39 +0530

    swift icon

---
+++ Swfitui/SwiftIcon.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftIcon.swift
+//  Swfitui
+//
+//  Created by Shiraz on 25/07/26.
+//
+
+import SwiftUI
+
+struct SwiftIcon: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftIcon()
+}
