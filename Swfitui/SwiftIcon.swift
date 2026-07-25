9a268a050950f130cbd8a8aaa7ee75d1515a30c8
shiraz877 <gm6564@myamu.ac.in>
2026-07-25 15:42:18 +0530

    swift image

--- Swfitui/SwiftIcon.swift
+++ Swfitui/SwiftIcon.swift
@@ -9,7 +9,18 @@ import SwiftUI
 
 struct SwiftIcon: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Image(systemName: "heart.fill")
+            .resizable() //this will increase the size of icon on the basis of frame
+            .foregroundColor(
+                .green
+            )
+//        aspectRatio(contentMode: .fit)
+//            .font(.title) //this is the system size
+//            .font(.system(size: 300)) this the manual way to scale the size of icon
+            .frame(width: 300, height: 300)
+        
+        Image(systemName: "circle")
+        
     }
 }
 
---
+++ Swfitui/SwiftImage.swift
@@ -0,0 +1,27 @@
+//
+//  SwiftImage.swift
+//  Swfitui
+//
+//  Created by Shiraz on 25/07/26.
+//
+
+import SwiftUI
+
+struct SwiftImage: View {
+    var body: some View {
+        Image("img") // to get this image first we have set the image in asset folder
+            .resizable() // this is for make size varaible
+            .frame(width: 300, height: 200) // frame gives you a skalaton
+            .clipShape(
+//                Circle() this is like profile pic
+//                Rectangle()
+//                RoundedRectangle(cornerRadius: 20)
+//                Ellipse()
+                
+            ) //this will give you shape of the image like circle
+    }
+}
+
+#Preview {
+    SwiftImage()
+}
